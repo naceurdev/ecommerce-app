66a839c37c09423fec018758678f588f2e6a5603
Naceur Ben Yahia <naceur.benyahia@oyez.fr>
2019-07-23 09:09:10 +0100

    add dotenv to front

--- front/src/index.js
+++ front/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import dotenv from 'dotenv';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+dotenv.config();
 
 ReactDOM.render(
   <BrowserRouter>
